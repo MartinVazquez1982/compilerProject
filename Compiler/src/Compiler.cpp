3d8d477491a3017084d796f58b7205a68dd23f77
joaquinBene <joaquinbenecier@hotmail.com>
2023-11-06 21:36:13 -0300

    crear archivo con lineas mismo path

--- Compiler/src/Compiler.cpp
+++ Compiler/src/Compiler.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "./AnalisisSintactico/Parse.h"
 #include "./ContErrWar/ContErrWar.h"
 #include "./AnalisisSemantico/EstructuraTercetos.h"
@@ -16,36 +17,56 @@
 #define YELLOW  "\x1B[33m"
 #define RED "\x1B[31m"
 
-using namespace std;
-
 //Archivo con el codigo fuente
 ifstream codigoFuente;
 
-/**
- * Main del compilador se revisa si hubo un path recibido como
- * parametro y si puede abrir dicho archivo. Luego realiza la compilacion
- */
+using namespace std;
+
 int main(int argc, char *argv[]) {
-	if (argc > 2) {
-		throw runtime_error("Demasiados argumentos.");
-	} else if (argc < 2){
-		throw runtime_error("Falta codigo fuente a compilar.");
-	}
+    if (argc != 2) {
+        cout << "Uso: " << argv[0] << " <nombre del archivo>" << endl;
+        return 1;
+    }
 
-	const char* nombreArchivo = argv[1];
-	codigoFuente.open(nombreArchivo);
+    const char* nombreArchivo = argv[1];
+    codigoFuente.open(nombreArchivo);
 
-	if (!codigoFuente.is_open()) {
-		throw runtime_error("No se ha podido abrir el codigo fuente.");
-	}
+    if (!codigoFuente.is_open()) {
+        cout << "No se pudo abrir el archivo de entrada." << endl;
+        return 1;
+    }
 
-	yyparse();
-	codigoFuente.close();
-	cout << endl << YELLOW << "Warnings - " << ContErrWar::getWarning() << "		" << RED << "Errores - " << ContErrWar::getError() << RESET << endl << endl;
-	/*if(ContErrWar::generarCodigo()){
-		EstructuraTercetos::mostrarTercetos();
-	}*/
-	TablaDeSimbolos::imprimir();
-	EstructuraTercetos::mostrarTercetos();
-	return 0;
+    string nombreArchivoEntrada = string(argv[1]);
+    size_t found = nombreArchivoEntrada.find_last_of("/\\");
+    string path = nombreArchivoEntrada.substr(0, found);
+
+    string nombreArchivoSalida = nombreArchivoEntrada.substr(found + 1) + " - con lineas.txt";
+
+    ofstream archivo_salida(path + "/" + nombreArchivoSalida);
+
+    if (archivo_salida.is_open()) {
+        string linea;
+        int numero_linea = 1;
+
+        while (getline(codigoFuente, linea)) {
+            archivo_salida << numero_linea << " " << linea << "\n";
+            numero_linea++;
+        }
+
+        cout << "Se ha generado el archivo con las líneas numeradas como: " << path + "/" + nombreArchivoSalida << endl;
+        codigoFuente.close();
+        codigoFuente.clear();
+        codigoFuente.open(nombreArchivo);
+        yyparse();
+        codigoFuente.close();
+        archivo_salida.close();
+
+        cout << endl << YELLOW << "Warnings - " << ContErrWar::getWarning() << "		" << RED << "Errores - " << ContErrWar::getError() << RESET << endl << endl;
+        TablaDeSimbolos::imprimir();
+        EstructuraTercetos::mostrarTercetos();
+    } else {
+        cout << "No se pudo abrir el archivo de salida." << endl;
+    }
+
+    return 0;
 }
